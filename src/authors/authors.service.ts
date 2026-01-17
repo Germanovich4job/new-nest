2fbb9ef022beac5cb8caa01a9381c2e23642da2e
Yuriy <germanovich4job@gmail.com>
2026-01-18 00:31:56 +0300

    s

--- src/authors/authors.service.ts
+++ src/authors/authors.service.ts
@@ -5,17 +5,22 @@ import {
   ReadManyAuthorsDTO,
   ReadManyAuthorsQueryDTO,
 } from './dto';
+import { PrismaService } from 'src/prisma';
 
 @Injectable()
 export class AuthorsService {
+  constructor(private readonly prisma: PrismaService) {}
   getMany(query: ReadManyAuthorsQueryDTO): Promise<ReadManyAuthorsDTO> {
-    throw new NotImplementedException(`Method not implemented ${query}`);
+    throw new NotImplementedException('А вот ты и попался, голубчик!');
   }
   getOne(authorId: string): Promise<ReadAuthorDTO> {
     throw new NotImplementedException(`Method not implemented ${authorId}`);
   }
-  create(data: CreateAuthorDTO): Promise<string> {
-    throw new NotImplementedException(`Method not implemented ${data}`);
+  async create(data: CreateAuthorDTO): Promise<string> {
+    const { id } = await this.prisma.author.create({
+      data,
+    });
+    return id;
   }
   update(authorId: string, data: CreateAuthorDTO): Promise<void> {
     throw new NotImplementedException(`Method not implemented ${data}`);
--- src/main.ts
+++ src/main.ts
@@ -14,5 +14,7 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
