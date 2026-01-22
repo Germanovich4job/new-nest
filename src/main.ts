1d80c19b17ce13c1dd05453f65c37b3fa70b1977
Yuriy <germanovich4job@gmail.com>
2026-01-23 00:10:48 +0300

    343

--- src/main.ts
+++ src/main.ts
@@ -15,8 +15,18 @@ async function bootstrap() {
   app.enableCors({
     origin: 'http://localhost:5173',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: [
+      'Origin',
+      'X-Requested-With',
+      'Content-Type',
+      'Accept',
+      'Authorization',
+    ],
     credentials: true,
+    optionSuccessStatus: 200,
   });
+
+  // app.setGlobalPrefix('api');
 
   await app.listen(process.env.PORT ?? 3000);
 }
